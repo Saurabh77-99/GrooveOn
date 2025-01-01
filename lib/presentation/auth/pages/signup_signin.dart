a5ba4d9f37d520a82442c163ce1299e5155ac78b
Saurabh Davda <saurabhdavda9977@gmail.com>
2025-01-01 11:18:44 +0530

    signup signin button added

--- lib/presentation/auth/pages/signup_signin.dart
+++ lib/presentation/auth/pages/signup_signin.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grooveon/common/widgets/button/basic_button.dart';
 import 'package:grooveon/core/configs/assets/app_images.dart';
 import 'package:grooveon/core/configs/assets/app_vectors.dart';
+import 'package:grooveon/core/configs/theme/app_colors.dart';
 
 class SignUpSignIn extends StatelessWidget {
   const SignUpSignIn({super.key});
@@ -27,6 +29,76 @@ class SignUpSignIn extends StatelessWidget {
             alignment: Alignment.bottomLeft,
             child: Image.asset(
               AppImages.authBG
+             ),
+          ),
+          
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AppVectors.logo
+                  ),
+                  const SizedBox(
+                    height: 55,
+                  ),
+                  const Text(
+                    'Enjoy Listening to Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 55,
+                  ),
+                  const Text(
+                    'Enjoy Listening to Music lorem ipsum ider',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13,
+                      color: AppColors.grey
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+              
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicButton(
+                          onPressed: (){}, 
+                          title: 'Register'
+                        ),
+                      ),
+                      const SizedBox(width: 20,),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: (){}, 
+                          child: const Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white
+                            ),
+                          )
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ],
