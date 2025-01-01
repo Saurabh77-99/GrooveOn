9b67e5c3e83c31f4553112e952e2016a01e81772
Saurabh Davda <saurabhdavda9977@gmail.com>
2025-01-01 10:56:57 +0530

    signup signin page images aligned added!

--- lib/presentation/auth/pages/signup_signin.dart
+++ lib/presentation/auth/pages/signup_signin.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grooveon/core/configs/assets/app_images.dart';
+import 'package:grooveon/core/configs/assets/app_vectors.dart';
 
 class SignUpSignIn extends StatelessWidget {
   const SignUpSignIn({super.key});
@@ -8,7 +11,24 @@ class SignUpSignIn extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(
+              AppVectors.topPattern
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(
+              AppVectors.bottomPattern
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(
+              AppImages.authBG
+            ),
+          ),
         ],
       ),
     );
